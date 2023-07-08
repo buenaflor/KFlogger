78fb143877493534ea222245ca585fb5e7dfcc20
Giancarlo Buenaflor <giancarlo_buenaflor@yahoo.com>
2023-07-08 03:04:33 +0200

    Adjust comments in JVM files

--- api/src/jvmMain/kotlin/com/buenaflor/kflogger/backend/KLogData.jvm.kt
+++ api/src/jvmMain/kotlin/com/buenaflor/kflogger/backend/KLogData.jvm.kt
@@ -5,45 +5,23 @@ import com.buenaflor.kflogger.KLogSite
 
 actual typealias KLogData = LogData
 
-/** Returns the log level for the current log statement. */
 actual val KLogData.level: KLevel?
   get() = level
 
-/** @deprecated Use timestampNanos */
 actual val KLogData.timestampMicros: Long
   get() = timestampMicros
 
-/** Returns a nanosecond timestamp for the current log statement. */
 actual val KLogData.timestampNanos: Long
   get() = timestampNanos
 
-/** Returns the logger name (which is usually a canonicalized class name) or `null` if not given. */
 actual val KLogData.loggerName: String?
   get() = loggerName
 
-/**
- * Returns the log site data for the current log statement.
- *
- * @throws IllegalStateException if called prior to the postProcess() method being called.
- */
 actual val KLogData.logSite: KLogSite?
   get() = logSite
 
-/**
- * Returns the arguments to be formatted with the message. Arguments exist when a `log()` method
- * with a format message and separate arguments was invoked.
- *
- * @throws IllegalStateException if no arguments are available (ie, when there is no template
- *   context).
- */
 actual val KLogData.arguments: Array<Any?>?
   get() = arguments
 
-/**
- * Returns the single argument to be logged directly when no arguments were provided.
- *
- * @throws IllegalStateException if no single literal argument is available (ie, when a template
- *   context exists).
- */
 actual val KLogData.literalArgument: Any?
   get() = literalArgument
--- api/src/jvmMain/kotlin/com/buenaflor/kflogger/backend/KMetadataKey.jvm.kt
+++ api/src/jvmMain/kotlin/com/buenaflor/kflogger/backend/KMetadataKey.jvm.kt
@@ -5,19 +5,8 @@ import com.buenaflor.kflogger.MetadataKey
 @Suppress("NO_ACTUAL_CLASS_MEMBER_FOR_EXPECTED_CLASS")
 actual typealias KMetadataKey<T> = MetadataKey<T>
 
-/**
- * Returns a short, human readable text label which will prefix the metadata in cases where it is
- * formatted as part of the log message.
- */
 actual val <T> KMetadataKey<T>.label: String
   get() = label
 
-/**
- * Returns a 64-bit bloom filter mask for this metadata key, usable by backend implementations to
- * efficiently determine uniqueness of keys (e.g. for deduplication and grouping). This value is
- * calculated on the assumption that there are normally not more than 10 distinct metadata keys
- * being processed at any time. If more distinct keys need to be processed using this Bloom Filter
- * mask, it will result in a higher than optimal false-positive rate.
- */
 actual val <T> KMetadataKey<T>.bloomFilterMask: Long
   get() = bloomFilterMask
