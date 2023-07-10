01e0184779229c02b630980241cc373f5a0c3446
buenaflor <giancarlo.buenaflor@sentry.io>
2023-07-10 17:09:16 +0200

    Convert LoggingApi.java to Kotlin JVM

--- api/src/jvmMain/kotlin/com/buenaflor/kflogger/KLoggingApi.jvm.kt
+++ api/src/jvmMain/kotlin/com/buenaflor/kflogger/KLoggingApi.jvm.kt
@@ -4,8 +4,6 @@ import com.buenaflor.kflogger.backend.KLoggerBackend
 
 actual typealias KLoggingApi<API> = LoggingApi<API>
 
-@Suppress("NO_ACTUAL_CLASS_MEMBER_FOR_EXPECTED_CLASS")
-actual typealias KLoggingApiNoOp<API> = LoggingApi.NoOp<API>
-
 actual val <API : KLoggingApi<API>> KAbstractLogger<API>.backend: KLoggerBackend
   get() = backend
+
