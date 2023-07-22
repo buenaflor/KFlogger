f35e31003eb33ea4cba0834d552fdcd6d170615b
buenaflor <giancarlo.buenaflor@sentry.io>
2023-07-22 18:30:38 +0200

    Add LogContext

---
+++ api/src/jvmMain/kotlin/com/buenaflor/kflogger/KLogContext.jvm.kt
@@ -0,0 +1,7 @@
+package com.buenaflor.kflogger
+
+@Suppress("NO_ACTUAL_CLASS_MEMBER_FOR_EXPECTED_CLASS")
+public actual typealias KLogContext<LOGGER, API> = LogContext<LOGGER, API>
+
+@Suppress("NO_ACTUAL_CLASS_MEMBER_FOR_EXPECTED_CLASS")
+public actual typealias KLogContextKey = LogContext.Key
