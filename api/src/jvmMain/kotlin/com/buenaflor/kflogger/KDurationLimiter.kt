773368df4e70fbb97e7389e874b46f491910f0e4
buenaflor <giancarlo.buenaflor@sentry.io>
2023-07-22 18:31:39 +0200

    Remove DurationRateLimiter for now

--- api/src/jvmMain/kotlin/com/buenaflor/kflogger/KDurationLimiter.kt
+++
@@ -1,7 +0,0 @@
-package com.buenaflor.kflogger
-
-@Suppress("ACTUAL_WITHOUT_EXPECT")
-internal actual typealias KDurationRateLimiter = DurationRateLimiter
-
-@Suppress("ACTUAL_WITHOUT_EXPECT")
-internal actual typealias KRateLimitPeriod = DurationRateLimiter.RateLimitPeriod
