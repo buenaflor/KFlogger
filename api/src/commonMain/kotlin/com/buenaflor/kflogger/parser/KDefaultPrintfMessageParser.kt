d3f113aca65776837d33c45e41a860a4c0fc042c
buenaflor <giancarlo.buenaflor@sentry.io>
2023-08-02 00:21:58 +0200

    Add PrintfMessageParser

---
+++ api/src/commonMain/kotlin/com/buenaflor/kflogger/parser/KDefaultPrintfMessageParser.kt
@@ -0,0 +1,25 @@
+package com.buenaflor.kflogger.parser
+
+/**
+ * Default implementation of the printf message parser. This parser supports all the place-holders
+ * available in `String#format` but can be extended, if desired, for additional behavior
+ * For consistency it is recommended, but not required, that custom printf parsers always extend
+ * from this class.
+ *
+ *
+ * This class is immutable and thread safe (and any subclasses must also be so).
+ */
+public expect class KDefaultPrintfMessageParser private constructor() : KPrintfMessageParser {
+    override fun parsePrintfTerm(
+        builder: KMessageBuilder<*>?,
+        index: Int,
+        message: String?,
+        termStart: Int,
+        specStart: Int,
+        formatStart: Int
+    ): Int
+
+    public companion object {
+        public fun getInstance(): KPrintfMessageParser?
+    }
+}
---
+++ api/src/jvmMain/kotlin/com/buenaflor/kflogger/parser/KDefaultPrintfMessageParser.jvm.kt
@@ -0,0 +1,5 @@
+package com.buenaflor.kflogger.parser
+
+// Suppress errors: companion object - Java static does not match companion object
+@Suppress("NO_ACTUAL_CLASS_MEMBER_FOR_EXPECTED_CLASS")
+public actual typealias KDefaultPrintfMessageParser = DefaultPrintfMessageParser
