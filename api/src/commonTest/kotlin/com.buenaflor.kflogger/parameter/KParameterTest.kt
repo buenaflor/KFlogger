bab38e5ab968ab6cc1041a127ebc36ac42cd4b5c
buenaflor <giancarlo.buenaflor@sentry.io>
2023-08-02 13:58:00 +0200

    Adjust class names in commonTest

--- api/src/commonTest/kotlin/com.buenaflor.kflogger/parameter/KParameterTest.kt
+++ api/src/commonTest/kotlin/com.buenaflor.kflogger/parameter/KParameterTest.kt
@@ -5,7 +5,7 @@ import com.buenaflor.kflogger.util.IgnoreIos
 import kotlin.test.Test
 
 class KParameterTest {
-  class CustomParameter : KParameter(KFormatOptions.getDefault(), 1) {
+  class CompileOnlyParameter : KParameter(KFormatOptions.getDefault(), 1) {
     override fun accept(visitor: KParameterVisitor, value: Any) {}
 
     override fun getFormat(): String {
@@ -18,7 +18,7 @@ class KParameterTest {
   @IgnoreIos
   fun testNotCrashing() {
     // This test is to ensure that the code compiles and does not crash.
-    val parameter = CustomParameter()
+    val parameter = CompileOnlyParameter()
     parameter.getFormat()
     parameter.index()
   }
--- api/src/commonTest/kotlin/com.buenaflor.kflogger/parser/KMessageParserTest.kt
+++ api/src/commonTest/kotlin/com.buenaflor.kflogger/parser/KMessageParserTest.kt
@@ -1,47 +1,20 @@
 package com.buenaflor.kflogger.parser
 
-import com.buenaflor.kflogger.backend.KFormatOptions
-import com.buenaflor.kflogger.backend.KTemplateContext
-import com.buenaflor.kflogger.backend.message
-import com.buenaflor.kflogger.parameter.KParameter
-import com.buenaflor.kflogger.parameter.KParameterVisitor
 import com.buenaflor.kflogger.util.IgnoreIos
 import kotlin.test.Test
 
 class KMessageParserTest {
-  val parser = TestKMessageParser()
-  val templateContext = KTemplateContext(parser, "message")
-  val messageBuilder = TestKMessageBuilder<String>(templateContext)
-  val parameter = TestKParameter(KFormatOptions.getDefault(), 0)
-
-  class TestKMessageParser : KMessageParser() {
+  private class CompileOnlyMessageParser : KMessageParser() {
     override fun <T> parseImpl(builder: KMessageBuilder<T>?) {}
 
     override fun unescape(out: StringBuilder?, message: String?, start: Int, end: Int) {}
-  }
-
-  class TestKMessageBuilder<T>(val templateContext: KTemplateContext) :
-      KMessageBuilder<String>(templateContext) {
-    override fun addParameterImpl(termStart: Int, termEnd: Int, param: KParameter) {}
-
-    override fun buildImpl(): String {
-      return "${templateContext.message} + 1"
-    }
-  }
-
-  class TestKParameter(options: KFormatOptions, index: Int) : KParameter(options, index) {
-    override fun accept(visitor: KParameterVisitor, value: Any) {}
-
-    override fun getFormat(): String {
-      return "format"
-    }
   }
 
   @Test
   @IgnoreIos
   fun testNotCrashing() {
     // This test is to ensure that the code compiles and does not crash.
-    val parser = TestKMessageParser()
+    val parser = CompileOnlyMessageParser()
     parser.unescape(null, null, 0, 0)
   }
 }
