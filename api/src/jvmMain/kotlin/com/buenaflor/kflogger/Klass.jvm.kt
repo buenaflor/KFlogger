8001b25b41614984d6df7d3c43c1f4d2fd8e420e
buenaflor <giancarlo.buenaflor@sentry.io>
2023-08-04 03:09:32 +0200

    Remove print

--- api/src/jvmMain/kotlin/com/buenaflor/kflogger/Klass.jvm.kt
+++ api/src/jvmMain/kotlin/com/buenaflor/kflogger/Klass.jvm.kt
@@ -5,7 +5,5 @@ import kotlin.reflect.KClass
 public actual typealias Klass<T> = Class<T>
 
 public actual fun <T : Any> KClass<T>.toKlass(): Klass<T> {
-  println(this.java.name)
-  println(this.java.canonicalName)
   return this.java
 }
